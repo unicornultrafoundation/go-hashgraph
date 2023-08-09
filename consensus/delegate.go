92d12c14bf6ce08fd0f5bbdb8dc7eb2611d7b61d
beland.developer <thangn.1411@gmail.com>
2023-08-09 15:34:00 +0700

    consensus: add process delegate function

--- consensus/delegate.go
+++ consensus/delegate.go
@@ -5,6 +5,10 @@ import (
 	"github.com/unicornultrafoundation/go-hashgraph/types"
 )
 
+// ProcessDelegates processes a list of delegate data and updates the state accordingly.
+// It iterates through each delegate in the list and invokes ProcessDelegate for each.
+// If an error occurs during processing, the function returns an error.
+// Otherwise, it returns the updated state after processing all delegates.
 func ProcessDelegates(s *state.State, delegates []*types.DelegateDto) (*state.State, error) {
 	var err error
 	for _, delegate := range delegates {
@@ -16,6 +20,13 @@ func ProcessDelegates(s *state.State, delegates []*types.DelegateDto) (*state.St
 	return s, nil
 }
 
+// ProcessDelegate processes a delegate and updates the state with delegation information.
+// It checks if the validator and delegator exist in the state and processes accordingly.
+// If the validator doesn't exist, the function returns the original state.
+// If the delegator doesn't exist, it appends a new delegator to the state.
+// If a delegation exists, it updates the existing delegation with additional amount.
+// Otherwise, it appends a new delegation to the state.
+// The function returns the updated state after processing the delegate.
 func ProcessDelegate(s *state.State, delegate *types.DelegateDto) (*state.State, error) {
 	validatorIdx, ok := s.ValidatorIndexByAddress(delegate.ValidatorAddress)
 	if !ok {
