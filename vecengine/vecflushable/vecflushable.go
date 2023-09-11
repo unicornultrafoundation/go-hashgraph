b1eb1105d7fe40e5b5ea560e637ecde388e3513e
Phat Luu <yadsendew@protonmail.com>
2023-09-12 01:14:46 +0700

    remove usage of go-ethereum

--- vecengine/vecflushable/vecflushable.go
+++ vecengine/vecflushable/vecflushable.go
@@ -3,7 +3,7 @@ package vecflushable
 import (
 	"errors"
 
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/unicornultrafoundation/go-u2u/libs/common"
 	"github.com/unicornultrafoundation/go-hashgraph/u2udb"
 )
 
